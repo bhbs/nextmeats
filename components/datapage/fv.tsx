62176dc50b23e225266ea1d5ca92212dc11be20b
bhbs <bhbstar.me@gmail.com>
2021-03-05 23:14:04 +0900

    Counter

--- components/datapage/fv.tsx
+++ components/datapage/fv.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Component = (): React.ReactElement => {
   return (
     <section>
       <img src="./logo" alt="" />
       <div>100% 100%</div>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </section>
+  );
+};
+
+const Counter = (): React.ReactElement => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, count * 10);
+  });
+  return (
+    <>
+      <div>count: {count}</div>
+    </>
   );
 };
 
