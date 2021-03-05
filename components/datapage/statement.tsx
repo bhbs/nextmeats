99f1adf6b4a396dc321c2600011ebf8d438329da
bhbs <bhbstar.me@gmail.com>
2021-03-06 01:20:07 +0900

    update statement

--- components/datapage/statement.tsx
+++ components/datapage/statement.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import styles from "./statement.module.scss";
 
 const Component = (): React.ReactElement => {
   return (
     <section>
-      <div>
-        <h2>データが裏付けるネクストミーツのインパクト</h2>
+      <div className={styles.box}>
+        <h2 style={{ lineHeight: "1.4" }}>
+          データが裏付ける、
+          <br />
+          ネクストミーツの
+          <br />
+          インパクト。
+        </h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eius
           facilis earum laudantium repudiandae explicabo neque reprehenderit
           voluptas a sint cumque, enim porro voluptatem quae, obcaecati amet
           nobis expedita aliquam?
         </p>
+        <div className={styles.bd}></div>
+        <div className={styles.bd}></div>
+        <div className={styles.bd}></div>
+        <div className={styles.bd}></div>
       </div>
       <div>↓</div>
     </section>
