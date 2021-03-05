437e31b96962f58217a732aed685f99028a77e02
bhbs <bhbstar.me@gmail.com>
2021-03-05 23:24:57 +0900

    star effect

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,24 +1,28 @@
-import React from "react";
 import Head from "next/head";
-import Layout from "../components/layout";
+import React from "react";
+import Carousel from "../components/datapage/carousel";
 import Data from "../components/datapage/data";
 import Footer from "../components/datapage/footer";
-import Carousel from "../components/datapage/carousel";
 import Fv from "../components/datapage/fv";
+import Stars from "../components/datapage/stars";
 import Statement from "../components/datapage/statement";
+import Layout from "../components/layout";
 
 const Index = (): React.ReactElement => {
   return (
-    <Layout languageCode="jp">
-      <Head>
-        <meta name="viewport" content="width=375px" />
-      </Head>
-      <Fv />
-      <Statement />
-      <Carousel />
-      <Data name="karubi" />
-      <Footer />
-    </Layout>
+    <div style={{ background: "black", color: "white" }}>
+      <Layout languageCode="jp">
+        <Head>
+          <meta name="viewport" content="width=375px" />
+        </Head>
+        <Stars />
+        <Fv />
+        <Statement />
+        <Carousel />
+        <Data name="karubi" />
+        <Footer />
+      </Layout>
+    </div>
   );
 };
 
