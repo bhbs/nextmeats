d7a753d4aee93991345a90d55c359590d9afd2d2
bhbs <bhbstar.me@gmail.com>
2021-03-06 00:42:14 +0900

    update fv

--- components/datapage/fv.tsx
+++ components/datapage/fv.tsx
@@ -1,31 +1,45 @@
 import React, { useEffect, useState } from "react";
+import styles from "./fv.module.scss";
 
 const Component = (): React.ReactElement => {
   return (
-    <section>
-      <img src="./logo" alt="" />
-      <div>100% 100%</div>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+    <section className={styles.fv}>
+      <img
+        src="./img/datapage/fv/logo.png"
+        style={{ display: "block", padding: "32px", boxSizing: "border-box" }}
+      />
+      <div className={styles.counters}>
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
     </section>
   );
 };
 
 const Counter = (): React.ReactElement => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(Math.floor(Math.random() * 100));
   useEffect(() => {
     setTimeout(() => {
-      setCount(count + 1);
+      setCount(count > 90 ? Math.floor(Math.random() * 90) : count + 7);
     }, count * 10);
   });
   return (
     <>
-      <div>count: {count}</div>
+      <div className={styles.counter}>{count}%</div>
     </>
   );
 };
