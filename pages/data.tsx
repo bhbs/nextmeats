7ae0c7dfeb2a96f6d9311f9644757af06d6feed1
bhbs <bhbstar.me@gmail.com>
2021-03-05 21:45:15 +0900

    fix

--- pages/data.tsx
+++ pages/data.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import Data from "../components/datapage/data";
 import Footer from "../components/datapage/footer";
-import Caroucel from "../components/datapage/caroucel";
+import Carousel from "../components/datapage/carousel";
 import Fv from "../components/datapage/fv";
 import Statement from "../components/datapage/statement";
 
@@ -15,8 +15,8 @@ const Index = (): React.ReactElement => {
       </Head>
       <Fv />
       <Statement />
-      <Caroucel />
-      <Data />
+      <Carousel />
+      <Data name="karubi" />
       <Footer />
     </Layout>
   );
