a7f4f0c8335563e66a6d480e3ee0518632f6eb75
bhbs <bhbstar.me@gmail.com>
2021-03-06 13:56:05 +0900

    pararax

--- pages/data.tsx
+++ pages/data.tsx
@@ -7,23 +7,27 @@ import Fv from "../components/datapage/fv";
 import Stars from "../components/datapage/stars";
 import Statement from "../components/datapage/statement";
 import Layout from "../components/layout";
+import styles from "./data.module.scss";
 
 export type Product = "karubi" | "harami" | "chicken";
 
 const Index = (): React.ReactElement => {
   const [product, setProduct] = useState<Product>("chicken");
   return (
-    <div style={{ background: "black", color: "white" }}>
+    <div style={{ background: "black", color: "white", paddingTop: "58px" }}>
       <Layout languageCode="jp">
         <Head>
           <meta name="viewport" content="width=375px" />
         </Head>
         <Stars />
-        <Fv />
-        <Statement />
-        <Carousel setProduct={setProduct} />
-        <Data product={product} />
-        <Footer />
+        <div className={styles.container}>
+          <Fv />
+          <Statement />
+          <Carousel setProduct={setProduct} />
+          <Data product={product} />
+          <Footer />
+          <footer>© 2021 Next Meats Co., Ltd.</footer>
+        </div>
       </Layout>
     </div>
   );
