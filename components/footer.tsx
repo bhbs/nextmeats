6ac0a213a65b43b5e76a2d95cb2dc90ad694564f
bhbs <bhbstar.me@gmail.com>
2021-06-14 01:02:08 +0900

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LanguageCode } from "../lib/language";
 import SnsLink from "./snsLink";
 
@@ -8,77 +8,86 @@ type Props = {
   path: string;
 };
 
-const Footer = ({ languageCode, path }: Props): React.ReactElement => (
-  <footer>
-    <div>
-      <p>
-        Country:{" "}
-        <Link href={path} locale="ja">
-          <a
-            style={{
-              fontWeight:
-                location.hostname === "nextmeats.co.jp" ? "bold" : "normal",
-            }}
+const Footer = ({ languageCode, path }: Props): React.ReactElement => {
+  const [hostname, setHostname] = useState("");
+  useEffect(() => {
+    setHostname(window.location.hostname);
+  }, []);
+  return (
+    <footer>
+      <div>
+        <p>
+          Country:{" "}
+          <Link href={path} locale="ja">
+            <a
+              style={{
+                fontWeight: hostname === "nextmeats.co.jp" ? "bold" : "normal",
+              }}
+            >
+              日本
+            </a>
+          </Link>{" "}
+          /{" "}
+          <Link href={path} locale="en-US">
+            <a
+              style={{
+                fontWeight: hostname === "nextmeats.us" ? "bold" : "normal",
+              }}
+            >
+              United States
+            </a>
+          </Link>{" "}
+          /{" "}
+          <Link href={path} locale="zh-TW">
+            <a
+              style={{
+                fontWeight: hostname === "nextmeats.tw" ? "bold" : "normal",
+              }}
+            >
+              台湾
+            </a>
+          </Link>
+        </p>
+        <p>
+          Language:{" "}
+          <Link
+            href={languageCode === "jp" ? path : `/ja/${path}`}
+            locale={false}
           >
-            日本
-          </a>
-        </Link>{" "}
-        /{" "}
-        <Link href={path} locale="en-US">
-          <a
-            style={{
-              fontWeight:
-                location.hostname === "nextmeats.us" ? "bold" : "normal",
-            }}
+            <a
+              style={{ fontWeight: languageCode === "jp" ? "bold" : "normal" }}
+            >
+              日本語
+            </a>
+          </Link>{" "}
+          /{" "}
+          <Link
+            href={languageCode === "en" ? path : `/en/${path}`}
+            locale={false}
           >
-            United States
-          </a>
-        </Link>{" "}
-        /{" "}
-        <Link href={path} locale="zh-TW">
-          <a
-            style={{
-              fontWeight:
-                location.hostname === "nextmeats.tw" ? "bold" : "normal",
-            }}
+            <a
+              style={{ fontWeight: languageCode === "en" ? "bold" : "normal" }}
+            >
+              English
+            </a>
+          </Link>{" "}
+          /{" "}
+          <Link
+            href={languageCode === "zh" ? path : `/zh/${path}`}
+            locale={false}
           >
-            台湾
-          </a>
-        </Link>
-      </p>
-      <p>
-        Language:{" "}
-        <Link
-          href={languageCode === "jp" ? path : `/ja/${path}`}
-          locale={false}
-        >
-          <a style={{ fontWeight: languageCode === "jp" ? "bold" : "normal" }}>
-            日本語
-          </a>
-        </Link>{" "}
-        /{" "}
-        <Link
-          href={languageCode === "en" ? path : `/en/${path}`}
-          locale={false}
-        >
-          <a style={{ fontWeight: languageCode === "en" ? "bold" : "normal" }}>
-            English
-          </a>
-        </Link>{" "}
-        /{" "}
-        <Link
-          href={languageCode === "zh" ? path : `/zh/${path}`}
-          locale={false}
-        >
-          <a style={{ fontWeight: languageCode === "zh" ? "bold" : "normal" }}>
-            中文
-          </a>
-        </Link>
-      </p>
-    </div>
-    <SnsLink languageCode={languageCode} color="black" />
-    <span>© 2021 Next Meats Co., Ltd.</span>
-  </footer>
-);
+            <a
+              style={{ fontWeight: languageCode === "zh" ? "bold" : "normal" }}
+            >
+              中文
+            </a>
+          </Link>
+        </p>
+      </div>
+      <SnsLink languageCode={languageCode} color="black" />
+      <span>© 2021 Next Meats Co., Ltd.</span>
+    </footer>
+  );
+};
 
 export default Footer;
